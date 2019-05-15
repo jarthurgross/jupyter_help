103ed1aa705f7524a3fe3c0d21b767baa63df09c
Jonathan A. Gross <jarthurgross@gmail.com>
2019-05-15 16:10:31 -0400

    Sanitize html and use proper table header

--- src/jupyter_helper/html_display.py
+++ src/jupyter_helper/html_display.py
@@ -1,6 +1,17 @@
 import itertools as it
+import re
 from IPython.display import display, HTML
 import sympy as sy
+
+def sanitize_for_html(text):
+    # Adapted from https://stackoverflow.com/a/6117124/1236650
+    rep = {'&': '&amp;',
+           '<': '&lt;',
+           '>': '&gt;'}
+    rep = dict((re.escape(k), v) for k, v in rep.items())
+    pattern = re.compile("|".join(rep.keys()))
+    text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+    return text
 
 def table_format(rows, format_specs=None, header_entries=None, header_format_specs=None):
     html = '<table>'
@@ -12,14 +23,14 @@ def table_format(rows, format_specs=None, header_entries=None, header_format_spe
             header_format_specs = it.repeat('')
         html += '<tr>'
         for entry, format_spec in zip(header_entries, header_format_specs):
-            html += '<td>' + format(entry, format_spec) + '</td>'
+            html += '<th>' + sanitize_for_html(format(entry, format_spec)) + '</th>'
         html += '</tr>'
     if format_specs is None:
         format_specs = it.repeat('')
     for row in rows:
         html += '<tr>'
         for entry, format_spec in zip(row, format_specs):
-            html += '<td>' + format(entry, format_spec) + '</td>'
+            html += '<td>' + sanitize_for_html(format(entry, format_spec)) + '</td>'
         html += '</td>'
     html += '</table>'
     return html
