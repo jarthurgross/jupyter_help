136d8b4a31b7b388c69ff9693ef0605f23b0d3b5
Jonathan A. Gross <jarthurgross@gmail.com>
2019-12-10 15:15:21 -0500

    Make kwarg processor change the default dict
    Before it was changing the user-supplied dict, which meant subsequent
    calls using the users dict would get messed up by the default values.

--- src/jupyter_helper/visualization.py
+++ src/jupyter_helper/visualization.py
@@ -9,10 +9,8 @@ def process_default_kwargs(kwargs, default_kwargs):
     """
     if kwargs is None:
         kwargs = {}
-    for kwarg, value in default_kwargs.items():
-        if kwarg not in kwargs:
-            kwargs[kwarg] = value
-    return kwargs
+    for kwarg, value in kwargs.items():
+        default_kwargs[kwarg] = value
 
 def heat_map(arr, XY=None, figure_kwargs=None, pcolormesh_kwargs=None):
     '''Plot a heat map of the values of an array with a colorbar.
@@ -21,24 +19,23 @@ def heat_map(arr, XY=None, figure_kwargs=None, pcolormesh_kwargs=None):
     vmin = arr.min()
     vmax = arr.max()
     default_figure_kwargs = {'figsize': (6, 4)}
-    figure_kwargs = process_default_kwargs(figure_kwargs,
-                                           default_figure_kwargs)
+    process_default_kwargs(figure_kwargs, default_figure_kwargs)
     default_pcolormesh_kwargs = {'rasterized': True,
                                  'shading': 'gouraud',
                                  'vmin': vmin,
                                  'vmax': vmax}
-    pcolormesh_kwargs = process_default_kwargs(pcolormesh_kwargs,
-                                               default_pcolormesh_kwargs)
-    fig = plt.figure(**figure_kwargs)
+    process_default_kwargs(pcolormesh_kwargs, default_pcolormesh_kwargs)
+    fig = plt.figure(**default_figure_kwargs)
     gs  = mpl.gridspec.GridSpec(1, 2, width_ratios=[1, 0.25])
     ax0 = plt.subplot(gs[0])
     ax1 = plt.subplot(gs[1])
     if XY is None:
-        cax = ax0.pcolormesh(arr, **pcolormesh_kwargs)
+        cax = ax0.pcolormesh(arr, **default_pcolormesh_kwargs)
     else:
-        cax = ax0.pcolormesh(*XY, arr, **pcolormesh_kwargs)
+        cax = ax0.pcolormesh(*XY, arr, **default_pcolormesh_kwargs)
     cbar = fig.colorbar(cax, cax=ax1)
-    cbar.set_ticks([pcolormesh_kwargs['vmin'], pcolormesh_kwargs['vmax']])
+    cbar.set_ticks([default_pcolormesh_kwargs['vmin'],
+                    default_pcolormesh_kwargs['vmax']])
     return fig, [ax0, ax1]
 
 def visualize_real_array(arr, figure_kwargs=None, matshow_kwargs=None):
@@ -49,22 +46,21 @@ def visualize_real_array(arr, figure_kwargs=None, matshow_kwargs=None):
     vmin = -vmax
 
     default_figure_kwargs = {'figsize': (6, 4)}
-    figure_kwargs = process_default_kwargs(figure_kwargs,
-                                           default_figure_kwargs)
+    process_default_kwargs(figure_kwargs, default_figure_kwargs)
 
     default_matshow_kwargs = {'cmap': mpl.cm.RdBu,
                               'vmin': vmin,
                               'vmax': vmax}
-    matshow_kwargs = process_default_kwargs(matshow_kwargs,
-                                            default_matshow_kwargs)
+    process_default_kwargs(matshow_kwargs, default_matshow_kwargs)
 
-    fig = plt.figure(**figure_kwargs)
+    fig = plt.figure(**default_figure_kwargs)
     gs  = mpl.gridspec.GridSpec(1, 2, width_ratios=[1, 0.25])
     ax0 = plt.subplot(gs[0])
     ax1 = plt.subplot(gs[1])
-    cax = ax0.matshow(arr, **matshow_kwargs)
+    cax = ax0.matshow(arr, **default_matshow_kwargs)
     cbar = fig.colorbar(cax, cax=ax1)
-    cbar.set_ticks([matshow_kwargs['vmin'], 0, matshow_kwargs['vmax']])
+    cbar.set_ticks([default_matshow_kwargs['vmin'], 0,
+                    default_matshow_kwargs['vmax']])
     return fig, [ax0, ax1]
 
 def visualize_complex_array(arr, figure_kwargs=None, matshow_kwargs=None):
@@ -77,24 +73,23 @@ def visualize_complex_array(arr, figure_kwargs=None, matshow_kwargs=None):
     vmax = max(np.abs(arr_real).max(), np.abs(arr_imag).max())
 
     default_figure_kwargs = {'figsize': (10, 4)}
-    figure_kwargs = process_default_kwargs(figure_kwargs,
-                                           default_figure_kwargs)
+    process_default_kwargs(figure_kwargs, default_figure_kwargs)
 
     default_matshow_kwargs = {'cmap': mpl.cm.RdBu,
                               'vmin': vmin,
                               'vmax': vmax}
-    matshow_kwargs = process_default_kwargs(matshow_kwargs,
-                                            default_matshow_kwargs)
+    process_default_kwargs(matshow_kwargs, default_matshow_kwargs)
 
-    fig = plt.figure(**figure_kwargs)
+    fig = plt.figure(**default_figure_kwargs)
     gs  = mpl.gridspec.GridSpec(1, 3, width_ratios=[1, 1, 0.25])
     ax0 = plt.subplot(gs[0])
     ax1 = plt.subplot(gs[1])
     ax2 = plt.subplot(gs[2])
-    ax0.matshow(arr_real, **matshow_kwargs)
-    cax = ax1.matshow(arr_imag, **matshow_kwargs)
+    ax0.matshow(arr_real, **default_matshow_kwargs)
+    cax = ax1.matshow(arr_imag, **default_matshow_kwargs)
     cbar = fig.colorbar(cax, cax=ax2)
-    cbar.set_ticks([matshow_kwargs['vmin'], 0, matshow_kwargs['vmax']])
+    cbar.set_ticks([default_matshow_kwargs['vmin'], 0,
+                    default_matshow_kwargs['vmax']])
     ax1.set_yticks([])
     return fig, [ax0, ax1, ax2]
 
@@ -114,10 +109,9 @@ def sphere_plot_lat_long(fn, lat_count=30, long_count=60, ax=None, cmap=None,
     default_plot_surface_kwargs = {'rasterized': True,
                                    'rcount': lat_count,
                                    'ccount': long_count}
-    plot_surface_kwargs = process_default_kwargs(plot_surface_kwargs,
-                                                 default_plot_surface_kwargs)
+    process_default_kwargs(plot_surface_kwargs, default_plot_surface_kwargs)
     default_norm_kwargs = {'vmin': vmin, 'vmax': vmax}
-    norm_kwargs = process_default_kwargs(norm_kwargs, default_norm_kwargs)
+    process_default_kwargs(norm_kwargs, default_norm_kwargs)
 
     return_figax = False
     if ax is None:
@@ -125,9 +119,9 @@ def sphere_plot_lat_long(fn, lat_count=30, long_count=60, ax=None, cmap=None,
         ax = fig.add_subplot(1, 1, 1, projection='3d')
         return_figax = True
     max_mag = np.abs(values).max()
-    norm = mpl.colors.Normalize(**norm_kwargs)
+    norm = mpl.colors.Normalize(**default_norm_kwargs)
     ax.plot_surface(Xs, Ys, Zs, facecolors=cmap(norm(values.real)),
-                    **plot_surface_kwargs)
+                    **default_plot_surface_kwargs)
     ax.set_aspect('equal')
     mpl.rcParams['savefig.dpi'] = 300
     return (fig, ax) if return_figax else None
